5cfbfeecc750d17f4cc9ee6c4b5d604a1111c1f1
tavaniagustina <atavani@frba.utn.edu.ar>
2023-05-29 16:00:36 -0300

    Terminado2

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,92 +3,79 @@
 --------------
 
 data Personaje = Personaje {
-    edad :: Int,
-    energia :: Float,
-    habilidades :: [Habilidad],
     nombre :: String,
+    edad :: Int,
+    energia :: Int,
+    habilidades :: [Habilidad],
     planeta :: String
 }
 
-data Guantele = Guantele {
+data Guantelete = Guantelete {
     material :: Material,
     gemas :: [Gema]
 }
 
-type Habilidad = String
 type Material = String
 type Gema = Personaje -> Personaje
+type Habilidad = String
 type Universo = [Personaje]
 
-chasquidoUniverso :: Guantele -> Universo -> Universo
-chasquidoUniverso unGuantele unUniverso
-    | puedeUsarse unGuantele = reducirMitad unUniverso
+chasquidoDelUniverso :: Guantelete -> Universo -> Universo
+chasquidoDelUniverso unGuantele unUniverso 
+    | puedeUsarse unGuantele = reducirPersonajesMitad unUniverso
     | otherwise              = unUniverso
 
-puedeUsarse :: Guantele -> Bool
-puedeUsarse unGuantele = ((==6) . length . gemas) unGuantele && ((== "uru") . material) unGuantele
+puedeUsarse :: Guantelete -> Bool
+puedeUsarse unGuantele = ((== 6) . length . gemas) unGuantele && ((== "uru") . material) unGuantele
 
-reducirMitad :: Universo -> Universo
-reducirMitad unUniverso = take (div (length unUniverso) 2) unUniverso
+reducirPersonajesMitad :: Universo -> Universo
+reducirPersonajesMitad unUniverso = take (div (length unUniverso) 2) unUniverso
 
 --------------
 -- Punto 02 --
 --------------
 
-aptoParaPendex :: Universo -> Bool
-aptoParaPendex = any $ (<= 45) . edad
+aptoPendex :: Universo -> Bool
+aptoPendex = any $ (<= 45) . edad  
 
-energiaTotal :: Universo -> Float
-energiaTotal = sum . map energia . filter ((> 1) . length . habilidades)
+energiaTotal :: Universo -> Int
+energiaTotal = sum . map energia . filter ((> 1) . length.  habilidades)
 
 --------------
 -- Punto 03 --
 --------------
 
-mente :: Float -> Gema
-mente unValor unPersonaje = unPersonaje { energia = energia unPersonaje - unValor }
+mente :: Int -> Gema
+mente unValor unPersonaje = unPersonaje { energia = energia unPersonaje - unValor}
 
-type HabilidadAEliminar = String
+alma :: String -> Gema
+alma unaHabilidad unPersonaje = mente 10 . quitarHabilidad unaHabilidad $ unPersonaje
 
-alma :: HabilidadAEliminar -> Gema
-alma unaHabilidad = mente 10 . eliminarHabilidad unaHabilidad 
+quitarHabilidad :: String -> Gema
+quitarHabilidad unaHabilidad unPersonaje = unPersonaje { habilidades = filter (/= unaHabilidad) $ habilidades unPersonaje }
 
-eliminarHabilidad :: HabilidadAEliminar -> Gema
-eliminarHabilidad unaHabilidad = mapHabilidad (filter (/= unaHabilidad))
-
-mapHabilidad :: ([Habilidad] -> [Habilidad]) -> Personaje -> Personaje
-mapHabilidad unaFuncion unPersonaje = unPersonaje { habilidades = unaFuncion (habilidades unPersonaje)}
-
-type Planeta = String
-
-espacio :: Planeta -> Gema
-espacio unPlaneta = cambiarPlaneta unPlaneta . mente 20 
-
-cambiarPlaneta :: Planeta -> Gema
-cambiarPlaneta unPlaneta unPersonaje = unPersonaje { planeta = unPlaneta} 
+espacio :: String -> Gema
+espacio unPlaneta unPersonaje = mente 20 unPersonaje {planeta = unPlaneta}
 
 poder :: Gema
-poder unPersonaje
-    | cantidadDeHabilidades unPersonaje = eliminarTodasLasHabilidades . energiaEnCero $ unPersonaje
-    | otherwise                         = energiaEnCero unPersonaje
+poder unPersonaje 
+    | cantidadHabilidadesMenorADos unPersonaje = sacarTodasLasHabilidades . quitarTodaLaEnergia $ unPersonaje
+    | otherwise                                = quitarTodaLaEnergia unPersonaje
 
-cantidadDeHabilidades :: Personaje -> Bool
-cantidadDeHabilidades = (<= 2) . length . habilidades
+cantidadHabilidadesMenorADos :: Personaje -> Bool
+cantidadHabilidadesMenorADos = (<= 2) . length . habilidades 
 
-eliminarTodasLasHabilidades :: Personaje -> Personaje
-eliminarTodasLasHabilidades unPersonaje = unPersonaje { habilidades = [] } 
+sacarTodasLasHabilidades :: Personaje -> Personaje
+sacarTodasLasHabilidades unPersonaje = unPersonaje { habilidades = [] }
 
-energiaEnCero :: Personaje -> Personaje
-energiaEnCero unPersonaje = unPersonaje { energia = 0 } 
+quitarTodaLaEnergia :: Personaje -> Personaje
+quitarTodaLaEnergia unPersonaje = unPersonaje { energia = 0 }
 
 tiempo :: Gema
-tiempo unPersonaje = mente 50 . mitadEdad $ unPersonaje
-
-mitadEdad :: Personaje -> Personaje
-mitadEdad unPersonaje = unPersonaje { edad = (max 18 . div (edad unPersonaje)) 2 }
+tiempo unPersonaje = mente 50 unPersonaje { edad = max 18 (div (edad unPersonaje) 2) } 
 
 gemaLoca :: Gema -> Gema
-gemaLoca unaGema = unaGema . unaGema
+gemaLoca gema = gema . gema
 
 --------------
 -- Punto 04 --
@@ -99,21 +86,21 @@ gemaLoca unaGema = unaGema . unaGema
 -- Punto 05 --
 --------------
 
-utilizar :: [Gema] -> Gema 
-utilizar gemas unPersonaje = foldr ($) unPersonaje $ gemas
+utilizar :: [Gema] -> Gema
+utilizar gemas unPersonaje = foldr ($) unPersonaje gemas
 
 --------------
 -- Punto 06 --
 --------------
 
-gemaMasPoderosa :: Personaje -> Guantele -> Gema
+gemaMasPoderosa :: Personaje -> Guantelete -> Gema
 gemaMasPoderosa unPersonaje unGuantele = gemaMasPoderosaDe unPersonaje $ gemas unGuantele
 
 gemaMasPoderosaDe :: Personaje -> [Gema] -> Gema
 gemaMasPoderosaDe _ [gema] = gema
-gemaMasPoderosaDe unPersonaje (gema1:gema2:gemas)
-    | (energia . gema1) unPersonaje < (energia . gema2) unPersonaje = gemaMasPoderosaDe unPersonaje (gema1:gemas)
-    | otherwise                                                     = gemaMasPoderosaDe unPersonaje (gema2:gemas)
+gemaMasPoderosaDe unPersonaje (gema1:gema2:gemas) 
+    | (energia . gema1) unPersonaje < (energia . gema2) unPersonaje = gemaMasPoderosaDe unPersonaje (gema2:gemas)
+    | otherwise                                                     = gemaMasPoderosaDe unPersonaje (gema1:gemas)
 
 --------------
 -- Punto 07 --
@@ -122,11 +109,12 @@ gemaMasPoderosaDe unPersonaje (gema1:gema2:gemas)
 infinitasGemas :: Gema -> [Gema]
 infinitasGemas gema = gema:(infinitasGemas gema)
 
-guanteleteDeLocos :: Guantele
-guanteleteDeLocos = Guantele "vesconite" (infinitasGemas tiempo)
+guanteleteDeLocos :: Guantelete
+guanteleteDeLocos = Guantelete "vesconite" (infinitasGemas tiempo)
 
-usoLasTresPrimerasGemas :: Guantele -> Personaje -> Personaje
+usoLasTresPrimerasGemas :: Guantelete -> Personaje -> Personaje
 usoLasTresPrimerasGemas guantelete = (utilizar . take 3. gemas) guantelete
 
--- gemaMasPoderosa punisher guanteleteDeLocos
--- usoLasTresPrimerasGemas guanteleteDeLocos punisher
+-- gemaMasPoderosa punisher guanteleteDeLocos --> Loopea
+-- usoLasTresPrimerasGemas guanteleteDeLocos punisher 
+        -- loopea ya que es una evaluacion perezosa y nunca termina con la lista infinitas de gemas 
